9a146e24e6934efc5f1bcb3ae29fde8147cba5e6
Thomas <thomas.e.mahne@gmail.com>
2024-04-18 19:08:45 +1000

    centered navbar icons on small screen

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 overscroll-none">
       {/* Banner at the top to display project is under development */}
       <div className="z-10 w-full max-w-5xl items-center place-content-center font-mono text-sm lg:flex">
-          <p className="fixed top-0 left-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
+          <p className="fixed top-0 left-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xls">
             Currently in development by&nbsp;
             <a href="https://github.com/thomahn3">
               <u>thomahn3</u>
@@ -19,7 +19,7 @@ export default function Home() {
       </div>
 
       <div>
-        <input type="text" className="flex justify-center border-2 border-solid rounded-md border-gray-300 bg-zinc-200 backdrop-blur-2xl text-zinc-800/30 dark:bg-zinc-800/30 dark:text-zinc-200 dark:from-inherit"/>
+        <input type="text" className="flex justify-center border-2 border-solid rounded-md border-gray-300 bg-zinc-200 backdrop-blur-2xl"/>
         <button></button>
       </div>
     </main>
