25b260c22d30bd06f5f79584d64c0f69390353c5
Thomas <thomas.e.mahne@gmail.com>
2024-04-25 16:17:13 +1000

    Restarted

--- nextjs-app/app/dashboard/kitchen/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-        <main>
-            <p>Food Page</p>
-        </main>
-    )
-}
--- nextjs-app/app/ui/button.tsx
+++
@@ -1,6 +0,0 @@
-/* Buttons to Join Email List, Social Medias*/
-import Link from "next/link";
-
-export function emailList() {
-     
-}
